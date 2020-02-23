21233456e5352ceadf7a2538413ec478ab2fea89
Hyukin Kown <901po3@gmail.com>
2020-02-23 14:02:04 -0500

    vibration added

--- Assets/Scripts/MiniGame.cs
+++ Assets/Scripts/MiniGame.cs
@@ -5,11 +5,21 @@ using UnityEngine.UI;
 
 public class MiniGame : MonoBehaviour
 {
-    public bool miniGameOn = false;  
+    public bool miniGameOn = false;
+    public GameObject lockPick;
+    public Vector3 lockPickPos;
+
+
+    AudioSource audio;
+    public bool isStuck = true;
+    public AudioClip LockpickingStuck;
+    public AudioClip LockpickingTurn;
 
     private void Awake()
     {
         gameObject.SetActive(false);
+        audio = GetComponent<AudioSource>();
+        lockPickPos = lockPick.transform.position;
     }
 
     public GameObject EKeyButton;
@@ -19,8 +29,47 @@ public class MiniGame : MonoBehaviour
         if(miniGameOn)
         {
             EKeyButton.SetActive(false);
+            LockPicking();
         }
     }
+
+    private void LockPicking()
+    {
+        lockPick.transform.eulerAngles = Vector3.forward * 180 * Mathf.Clamp((Input.mousePosition.x / Screen.width), 0.01f, 0.99f);
+        lockPick.transform.eulerAngles = Vector3.forward * Mathf.Clamp(lockPick.transform.eulerAngles.z, 0, 180);
+
+        if(isStuck)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (audio.clip != LockpickingStuck)
+                {
+                    audio.clip = LockpickingStuck;
+                    audio.Play();
+                }
+            }
+            else if (Input.GetKeyUp(KeyCode.E))
+            {
+                if (audio.clip == LockpickingStuck && audio.isPlaying)
+                {
+                    audio.Stop();
+                    lockPick.transform.position = lockPickPos;
+                    audio.clip = null;
+                }
+            }
+            else if(Input.GetKey(KeyCode.E))
+            {
+                Vibration();
+            }
+        }
+    }
+
+    private void Vibration()
+    {
+        Vector3 temp = new Vector3(lockPickPos.x + Random.Range(-5, 5), lockPickPos.y + Random.Range(-5, 5), lockPickPos.z);
+        lockPick.transform.position = Vector3.Lerp(lockPick.transform.position, temp, Time.deltaTime * 50);
+    }
+
 
     public void AbortButton()
     {
